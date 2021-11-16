414085b20ec0f0bf52d32374e77ec86abf936bb8
Darienken <diegorpro2024@gmail.com>
2021-11-15 20:53:12 -0500

    Django Mails Completed

---
+++ PortfolioApp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+# Create your views here.
+
+
+def MainPage(request):
+    
+    if request.method=="POST":
+
+        affair=request.POST["contact-me__input-affair"]
+        name=request.POST["contact-me__input-name"]
+        last_name=request.POST["contact-me__input-lastname"]
+        message=request.POST["contact-me__input-message"]
+        tel=request.POST["contact-me__input-tel"]
+        mail=request.POST["contact-me__input-mail"]
+        body=f"Name: {name} LastName: {last_name} Mail: {mail} Tel: {tel}\n{message}"
+        
+        send_mail(affair,body,settings.EMAIL_HOST_USER,["diegorpro2024@gmail.com"])
+
+        return render(request, "MainPage.html")
+
+    
+    return render(request,"MainPage.html")
