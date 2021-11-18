633eb067a2668570967367a86d5a29d41ff0de83
Didackmon <diegorpro2024@gmail.com>
2021-11-18 08:28:24 -0500

    PortfolioV1 Finished

--- PortfolioProject/urls.py
+++ PortfolioProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from PortfolioApp.views import MainPage
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPage)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
