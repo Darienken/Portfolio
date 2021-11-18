d9dcb3a374ae16b41f4b27a2891dc2fdfb175fc0
Didackmon <diegorpro2024@gmail.com>
2021-11-18 10:52:24 -0500

    PortfolioV1

--- PortfolioProject/settings.py
+++ PortfolioProject/settings.py
@@ -80,17 +80,11 @@ WSGI_APPLICATION = 'PortfolioProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
 }
 
 # Password validation
