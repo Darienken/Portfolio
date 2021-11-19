017a86f11c9d1e4bff1587297cb6b38b899365bc
Didackmon <diegorpro2024@gmail.com>
2021-11-19 08:43:48 -0500

    My Portfolio

--- PortfolioProject/settings.py
+++ PortfolioProject/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'PortfolioProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "PortfolioApp" , "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
