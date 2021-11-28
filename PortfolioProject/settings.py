9cf294a13b30f8d1c7d72466a0ca23654a80bd78
Didackmon Pyshis <diegorpro2024gmail.com>
2021-11-28 10:04:53 -0500

    PortfolioV1

--- PortfolioProject/settings.py
+++ PortfolioProject/settings.py
@@ -145,10 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email Config
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST=config('EMAIL_HOST', default='localhost')
+EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER=config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_USER="diegorpro2024@gmail.com"
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD', default='')
 
 django_heroku.settings(locals())
